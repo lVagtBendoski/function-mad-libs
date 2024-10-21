e6f8b841f33e241d3a44c6c37b74c6db49a5bedb
lVagtBendoski <157419244+lVagtBendoski@users.noreply.github.com>
2024-10-21 10:31:32 -0400

    Add files via upload

---
+++ Mad Libs.js
@@ -0,0 +1,14 @@
+madLibsText = document.getElementById('madLibsText');
+
+generateMadLibs = () =>{
+    console.log('function called');
+    typeOfBird=document.getElementById('typeOfBird').value;
+    roomateName=document.getElementById('roomateName').value;
+    noun=document.getElementById('noun').value;
+    liquid=document.getElementById('liquid').value;
+    bodyPart=document.getElementById('bodyPart').value;
+    console.log(typeOfBird);
+    const text = document.createElement('p');
+    text = 'teste';
+    madLibsText = text;
+}
